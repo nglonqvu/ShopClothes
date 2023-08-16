833a39b0ceb87fdb9c2bdf8909360569181b2756
ng.lonqvu <longvu131102@gmail.com>
2023-08-17 02:46:23 +0700

    update product detail

--- OnlineShop/Models/Product.cs
+++ OnlineShop/Models/Product.cs
@@ -14,8 +14,8 @@ namespace OnlineShop.Models
         public string Name { get; set; }
         public int? CateId { get; set; }
         public decimal? Price { get; set; }
-        public string Image { get; set; }
-        public string Description { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
         public int? Status { get; set; }
         public int? Quantity { get; set; }
 
--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<PRN211_BL5Context>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("MyConnect"));
+});
 
+builder.Services.AddSession(opt =>
+{
+    opt.IdleTimeout = TimeSpan.FromMinutes(10);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
