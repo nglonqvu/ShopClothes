dfb8c6851021614e930c960842daa90ef4b0f92b
ng.lonqvu <longvu131102@gmail.com>
2023-08-23 15:04:48 +0700

    fix

---
+++ OnlineShop/OnlineShop/DataAccess/DAO/ProductDAO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShop.DataAccess.DAO
+{
+    public class ProductDAO
+    {
+        public async Task<IEnumerable<Product>> GetProductList()
+        {
+            var products = new List<Product>();
+            try
+            {
+                using (var context = new PRN211_BL5Context())
+                {
+                    products = await context.Products.Include(p => p.Cate).OrderBy(p => p.ProductId).ToListAsync();
+                    return products;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }    
+        } 
+    }
+}
---
+++ OnlineShop/OnlineShop/Models/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models
+{
+    public class RegisterModel
+    {
+        [Required] 
+        public string? FullName { get; set; }
+
+        [Required] 
+        [EmailAddress] 
+        public string? Email { get; set; }
+
+        [Required] 
+        [DataType(DataType.Password)] 
+        public string? Password { get; set; }
+    }
+}
