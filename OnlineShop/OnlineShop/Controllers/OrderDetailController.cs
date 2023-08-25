4f699a9bc500bbd35835a8b4b3b99e6d5514a645
vanduongtran <duong.tranvan202@gmail.com>
2023-08-25 09:57:51 +0700

    checkout

---
+++ OnlineShop/OnlineShop/Controllers/OrderDetailController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.DataAccess;
+
+namespace OnlineShop.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        private PRN211_BL5Context _context = new PRN211_BL5Context();
+        public IActionResult Index(int? id)
+        {
+            List<OrderDetail> orderDetails = _context.OrderDetails.Where(x => x.OrderId == id).ToList();
+            foreach (OrderDetail orderDetail in orderDetails)
+            {
+                orderDetail.ProductDetail = _context.ProductDetails.Where(x => x.ProductDetailId == orderDetail.ProductDetailId).SingleOrDefault();
+            }
+            ViewBag.list = orderDetails;
+            return View();
+        }
+    }
+}
